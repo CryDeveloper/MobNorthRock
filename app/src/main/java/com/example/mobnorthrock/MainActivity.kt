9aee84f7364efeb5b5d6d8688c27e6900b52878d
CryDeveloper <fordefdevat@gmail.com>
2023-03-23 23:29:05 +0300

    сделана главная страница

--- app/src/main/java/com/example/mobnorthrock/MainActivity.kt
+++ app/src/main/java/com/example/mobnorthrock/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.mobnorthrock
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+    fun init()
+    {
+        var day1 = findViewById<TextView>(R.id.day_one)
+        var day2 = findViewById<TextView>(R.id.day_two)
+
+        day1.setTextColor(this@MainActivity.getColor(R.color.white))
+        day2.setTextColor(this@MainActivity.getColor(R.color.white))
     }
 }
